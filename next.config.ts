1cc52a057d2f350f42b9e9740f1e615b573b3196
adityoarr <adityo.rafiu.d.din@gmail.com>
2026-04-09 13:40:58 +0700

    optimasi security

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // 1. Menyembunyikan identitas bahwa web ini dibuat dengan Next.js
+  poweredByHeader: false,
+
+  // 2. Mencegah orang asing membaca struktur kode asli Anda
+  productionBrowserSourceMaps: false,
+
+  // 3. Membatasi dari mana gambar boleh diambil (Mencegah SSRF)
   images: {
     remotePatterns: [
       {
@@ -9,6 +17,29 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
       },
     ],
+  },
+
+  // 4. Menambahkan Security Headers dasar
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Terapkan ke semua halaman web
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN', // Mencegah web Anda "dibajak" ke dalam frame web lain
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload', // Memaksa pengunjung menggunakan koneksi aman (HTTPS)
+          },
+        ],
+      },
+    ];
   },
 };
 
